990fff1e879ca2b51f1e4621c6c660abeec2068c
Cacperior <k.trz94@gmail.com>
2024-02-05 21:24:18 +0100

    Poprawki

---
+++ cypress/e2e/testy.cy.js
@@ -0,0 +1,18 @@
+import LoginPage from '../pages/LoginPage';
+import HomePage from '../pages/HomePage';
+
+describe('Testy logowania na stronie edu.goit.global', () => {
+  it('Powinien zalogować użytkownika i wylogować - Test 1', () => {
+    LoginPage.visit();
+    LoginPage.login('user888@gmail.com', '1234567890');
+    LoginPage.logout();
+    LoginPage.verifyUrl();
+  });
+
+  it('Powinien zalogować użytkownika i wylogować - Test 2', () => {
+    LoginPage.visit();
+    LoginPage.login('testowyqa@qa.team', 'QA!automation-1');
+    LoginPage.logout();
+    LoginPage.verifyUrl();
+  });
+});
--- cypress/pages/HomePage.js
+++ cypress/pages/HomePage.js
@@ -1,4 +1,12 @@
 class HomePage {
-  }
-  
-  export default new HomePage();
+    visit() {
+        cy.visit('https://www.edu.goit.global/');
+      }
+
+      logout() {
+        cy.get('#open-navigation-menu-mobile').click();
+        cy.contains('Log out').click();
+      }
+}
+
+export default new HomePage();
